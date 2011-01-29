53f41ba132bfb1333e132f8a1524631e5791730c
niki <niki@ab020f40-f023-40a1-80eb-922e57488772>
2011-01-29 03:30:32 +0000

    git-svn-id: http://www.aplusbi.com/ggj2011/trunk@4 ab020f40-f023-40a1-80eb-922e57488772

---
+++ Extinction/Extinction/Extinction/Cell.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Extinction
+{
+    abstract class Cell
+    {
+        int[,] grid;
+        int id = -1;
+        public Cell(int i, int[,] g)
+        {
+            id = i;
+            grid = g;
+        }
+
+        public abstract void Update(GameTime gameTime, int x, int y);
+        public abstract void Draw(int x, int y);
+    }
+}
--- Extinction/Extinction/Extinction/Program.cs
+++ Extinction/Extinction/Extinction/Program.cs
@@ -10,7 +10,7 @@ namespace Extinction
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (ExtGame game = new ExtGame())
             {
                 game.Run();
             }
